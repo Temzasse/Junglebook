8e65270498b2d6904683ab4326e44634fc7022f6
Teemu Taskula <teemu.taskula@aalto.fi>
2015-02-12 22:09:33 +0200

    bugsss

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,7 @@ from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.heroku import Heroku
 
 app = Flask(__name__)
-app.config.from_object('config')
+#app.config.from_object('config')
 heroku = Heroku(app)
 db = SQLAlchemy(app)
 
